b82786ef115620470b01f9565608160a055c053d
Yutao Zhang <stack@live.cn>
2013-03-06 16:29:48 +0800

    init

---
+++ nyan.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<path:url>')
+def nyan(url=""):
+	return render_template("nyan.html",url=url)
+
+@app.route('/caigu')
+def caigu():
+	return render_template("caigu.html")
+
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
