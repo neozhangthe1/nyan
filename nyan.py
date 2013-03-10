36699b96f1ae97eaa1961c10b0a8ea234bae8ac2
Yutao Zhang <stack@live.cn>
2013-03-11 04:34:11 +0800

    haha

--- nyan.py
+++ nyan.py
@@ -1,18 +1,37 @@
 from flask import Flask, render_template
 import os
-port = int(os.environ.get('PORT', 5000))
-app = Flask(__name__)
+from flask.ext.mail import Mail,Message
 
-@app.route('/')
-@app.route('/<path:url>')
-def nyan(url=""):
-	return render_template("nyan.html",url=url)
+port = int(os.environ.get('PORT', 3000))
+app = Flask(__name__)
+mail = Mail()
+
+app.config.update(
+	#Email Settings
+	MAIL_SERVER = "smtp.live.com",
+	MAIL_USE_TLS = True,
+	MAIL_USE_SSL = False,
+	MAIL_USERNAME = "xiaoyan@capricc.io",
+	MAIL_PASSWORD = "910915"
+	)
+mail.init_app(app)
+
+# @app.route('/nyan/')
+# @app.route('/nyan/<path:url>')
+# def nyan(url=""):
+# 	return render_template("nyan.html",url=url)
 
 @app.route('/caigu')
 def caigu():
 	return render_template("caigu.html")
 
+@app.route('/send/<content>')
+def send(content):
+	msg = Message(content,sender="xiaoyan@capricc.io",recipients=["yutao@capricc.io"])
+	msg.body=content
+	mail.send(msg)
+	return content
 
 if __name__ == "__main__":
-	#app.debug = True
+	app.debug = True
 	app.run(host='0.0.0.0',port=port)
